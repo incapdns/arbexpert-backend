5ebd0943de8cd71c17a38a9b390e6aad4c824d5b
Lucas Paixão <lucas.paixao@suprasoft.net>
2025-07-18 23:24:16 -0300

    Diminuir limite

--- src/exchange/mexc/public/watch.rs
+++ src/exchange/mexc/public/watch.rs
@@ -22,7 +22,7 @@ impl MexcExchange {
       .public
       .sub_clients
       .iter_mut()
-      .find(|c| c.subscribed_count() < 200)
+      .find(|c| c.subscribed_count() < 50)
     {
       return client.watch(symbol).await;
     }
