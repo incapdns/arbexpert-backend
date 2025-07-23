a1b4cc59171e9d05a5ab354350a300f08c7f36d6
Lucas Paixão <lucas.paixao@suprasoft.net>
2025-07-23 13:24:31 -0300

    Change limit

--- src/exchange/binance/public/sub_client.rs
+++ src/exchange/binance/public/sub_client.rs
@@ -282,9 +282,9 @@ impl BinanceSubClient {
     let server_time = now + time_offset_ms;
 
     let send_limiter = Arc::new(
-      Ratelimiter::builder(3, Duration::from_secs(1))
-        .max_tokens(3)
-        .initial_available(3)
+      Ratelimiter::builder(5, Duration::from_secs(5))
+        .max_tokens(5)
+        .initial_available(5)
         .alignment(Alignment::Second)
         .sync_time(server_time as u64)
         .build()
