a65d027167a1975194d4a426b274e48f1dd6573d
Lucas Paixão <lucas.paixao@suprasoft.net>
2025-07-23 04:38:49 -0300

    Fix

--- src/exchange/binance/public/sub_client.rs
+++ src/exchange/binance/public/sub_client.rs
@@ -282,9 +282,9 @@ impl BinanceSubClient {
     let server_time = now + time_offset_ms;
 
     let send_limiter = Arc::new(
-      Ratelimiter::builder(5, Duration::from_secs(1))
-        .max_tokens(5)
-        .initial_available(5)
+      Ratelimiter::builder(3, Duration::from_secs(1))
+        .max_tokens(3)
+        .initial_available(3)
         .alignment(Alignment::Second)
         .sync_time(server_time as u64)
         .build()
